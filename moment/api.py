b892e4d6891da7ed25d2dddd9e59a32927142b02
Zach Williams <hey@zachwill.com>
2012-12-18 17:12:07 -0800

    LOL

--- moment/api.py
+++ moment/api.py
@@ -10,16 +10,16 @@ def date(time, formula=None):
     return Moment(time, formula)
 
 
-def now(self):
+def now():
     """Create a date from the present time."""
     return Moment().now()
 
 
-def utc(self, date=None, formula=None):
+def utc(date=None, formula=None):
     """Create a date using the UTC time zone."""
     return Moment().utc(date, formula)
 
 
-def unix(self, timestamp):
+def unix(timestamp):
     """Create a date from a Unix timestamp."""
     return Moment().unix(timestamp)
