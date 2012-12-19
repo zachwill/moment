96e95208df4adbaa620cd421669d686da71aa054
Zach Williams <hey@zachwill.com>
2012-12-18 17:06:38 -0800

    Add a setup.py file

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""
+Setup and installation for the package.
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name="moment",
+    version="0.0.1",
+    url="http://github.com/zachwill/moment",
+    author="Zach Williams",
+    author_email="hey@zachwill.com",
+    description="Dealing with dates and times should be easy",
+    keywords=['moment', 'dates', 'times', 'zachwill'],
+    packages=[
+        'moment'
+    ],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
