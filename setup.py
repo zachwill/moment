26a5de65dbcc2e4771fc1cd1874b56c8c4373f3f
Zach Williams <hey@zachwill.com>
2012-12-20 22:32:00 -0800

    Update version now that properties are available

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name="moment",
-    version="0.0.7",
+    version="0.0.8",
     url="http://github.com/zachwill/moment",
     author="Zach Williams",
     author_email="hey@zachwill.com",
