9ed81476149fdc716f63a9a85a4f4fae72314286
Zach Williams <hey@zachwill.com>
2013-05-19 00:24:34 -0700

    Update version again

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name="moment",
-    version="0.1.1",
+    version="0.1.2",
     url="http://github.com/zachwill/moment",
     author="Zach Williams",
     author_email="hey@zachwill.com",
