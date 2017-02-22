0c98c36999655bb4419d36c9ec71c9a67369cfc9
Zach Williams <hey@zachwill.com>
2017-02-22 07:33:50 -0800

    Bump the version after `epoch` fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name="moment",
-    version="0.6",
+    version="0.6.1",
     url="http://github.com/zachwill/moment",
     author="Zach Williams",
     author_email="hey@zachwill.com",
